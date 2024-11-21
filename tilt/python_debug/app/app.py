88ad75736143c55004aa5f0bcfc6a3438229b8f4
vladmalynych <vladmalynych@gmail.com>
2024-11-21 12:19:10 +0100

    Update

--- tilt/python_debug/app/app.py
+++ tilt/python_debug/app/app.py
@@ -1,14 +1,10 @@
-from random import choice
-from flask import Flask, render_template, jsonify
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    import web_pdb; web_pdb.set_trace()
-
+    # import web_pdb; web_pdb.set_trace()
     return render_template("index.html")
 
-
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(port=8000, use_reloader=True)

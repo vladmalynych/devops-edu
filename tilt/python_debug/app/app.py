1faaf4713350692274ef6b8e69db8be12dc862eb
vladmalynych <vladmalynych@gmail.com>
2024-11-21 13:13:46 +0100

    Udpdate debugg

--- tilt/python_debug/app/app.py
+++ tilt/python_debug/app/app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+# import debugpy
+# debugpy.listen(("0.0.0.0", 5555))
+# print("Debugger is listening on port 5555. Attach your debugger to continue.")
+# debugpy.wait_for_client()
+# debugpy.breakpoint()
+
 app = Flask(__name__)
 
 @app.route("/")
